e0f9a187e37f11d0bdb451ca0c959fc9f4237133
Suraj <su.suraj.upadhyay@gmail.com>
2020-05-23 17:05:34 -0400

    Task 2: Program 5: Printing Nubmers --> Completed!

---
+++ C_Task2/src/com/java/operators/_5_PrintNumbers.java
@@ -0,0 +1,14 @@
+package com.java.operators;
+
+public class _5_PrintNumbers
+{
+	public static void main(String[] args)
+	{
+		System.out.print(0 + " ");
+		for(int i = 1; i < 6; i++)
+		{
+			if(i % 3 != 0) System.out.print(i + " ");
+		}
+	}
+
+}
