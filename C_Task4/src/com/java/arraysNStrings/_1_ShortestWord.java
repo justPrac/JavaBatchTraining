c236341897b61a7f0d34d7fb9d084f63126efbd9
Suraj <su.suraj.upadhyay@gmail.com>
2020-05-27 00:22:32 -0400

    T4: P1: completed!

--- C_Task4/src/com/java/arraysNStrings/_1_ShortestWord.java
+++ C_Task4/src/com/java/arraysNStrings/_1_ShortestWord.java
@@ -4,7 +4,23 @@ public class _1_ShortestWord
 {
 	public static void main(String[] args)
 	{
-		
+		String[] arr = {"one", "three", "hello", "13429"};
+		System.out.println("The min length in the String arr is: " + minLength(arr));
+	}
+	
+	private static int minLength(String [] arr)
+	{
+		int min = 0;
+		if(arr.length > 0)
+		{
+			min = arr[0].length();
+			
+			for(String s: arr)
+			{
+				if(min > s.length()) min = s.length();
+			}
+		}
+		return min;
 	}
 
 }
