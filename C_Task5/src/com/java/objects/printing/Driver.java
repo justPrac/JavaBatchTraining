7fa3f3c1d6f219c4565073c301949c35490d62b4
Suraj <su.suraj.upadhyay@gmail.com>
2020-05-27 17:26:11 -0400

    T5: P6: completed!

---
+++ C_Task5/src/com/java/objects/printing/Driver.java
@@ -0,0 +1,13 @@
+package com.java.objects.printing;
+
+public class Driver
+{
+
+	public static void main(String[] args)
+	{
+		new Programming();
+		new Programming("java");
+		new Programming("movies");
+	}
+
+}
---
+++ C_Task5/src/com/java/objects/printing/Programming.java
@@ -0,0 +1,14 @@
+package com.java.objects.printing;
+
+public class Programming
+{
+	public Programming()
+	{
+		System.out.println("I love programming languages.");
+	}
+	
+	public Programming(String s)
+	{
+		System.out.println("I love " + s + ".");
+	}
+}
